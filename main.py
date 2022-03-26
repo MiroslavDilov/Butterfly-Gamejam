9957c94a68994331a05d26f57dd25e30c03d8911
Miroslav Dilov <mirkos354@abv.bg>
2022-03-26 23:58:47 +0200

    Started the mouse mechanic

--- main.py
+++ main.py
@@ -2,7 +2,25 @@ import pygame
 
 pygame.init()
 
-screen = pygame.
-
+window = pygame.display.set_mode((600,600))
+window.fill((0,0,0))
 running = True
+
+class Frog:
+    def __init__(self):
+        self.rect = pygame.rect.Rect(0,0,50,50)
+
+    def update(self):
+        self.rect.midtop = pygame.mouse.get_pos()
+        pygame.draw.rect(window, (0,0,0), self.rect)
+
+frog = Frog()
+
 while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    window.fill((255,255,255))
+    frog.update()
+
+    pygame.display.flip()
